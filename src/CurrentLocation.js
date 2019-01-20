b34bfde3b326d02a5e1612e67850aed61b08058e
Rashad Balashov <rashad.balashov@gmail.com>
2019-01-20 15:16:19 -0500

    Getting Coordinates Through navigator

--- src/CurrentLocation.js
+++ src/CurrentLocation.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class CurrentLocation extends Component {
     
     state = {
-        location: null
+        location: null,
+        latitude: null,
+        longitude: null
     }
     
     handleSubmit = (e) => {
@@ -17,13 +19,47 @@ class CurrentLocation extends Component {
         })
     }
 
+    // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+    options = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+    }
+
+    success = (pos) => {
+        var currentCoords = pos.coords;
+        
+        this.setState({
+            latitude: currentCoords.latitude,
+            longitude: currentCoords.longitude
+        })
+        
+        console.log('Your current position is:');
+        console.log(`Latitude : ${currentCoords.latitude}`);
+        console.log(`Longitude: ${currentCoords.longitude}`);
+        console.log(`More or less ${currentCoords.accuracy} meters.`);
+    }
+
+    error = (err) => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    handleRequest = (e) => {
+        e.preventDefault();
+        navigator.geolocation.getCurrentPosition(this.success, this.error, this.options);
+    }
+
     render() {
         return(
-            <form id="currentLocationForm" onSubmit={ this.handleSubmit }>
-                <label htmlFor="location">ZipCode: </label>
-                <input type="text" id="location" onChange= { this.handleChange }/>
-                <button>Submit</button>
-            </form>
+            <div>
+                <form id="currentLocationForm" onSubmit={ this.handleSubmit }>
+                    <label htmlFor="location">ZipCode: </label>
+                    <input type="text" id="location" onChange= { this.handleChange }/>
+                    <button>Submit</button>
+                </form>
+                <form id="currentLocationRequest" onSubmit= { this.handleRequest }>
+                    <button>Request Location</button>
+                </form>
+            </div>
         )
     }
 }
