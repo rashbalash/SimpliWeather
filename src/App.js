20cf683cdff78a42f2bf78b66a5f16894779f18b
Rashad Balashov <rashad.balashov@gmail.com>
2019-01-20 19:34:36 -0500

    OpenWeatherMap API Working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import CurrentLocation from './CurrentLocation';
+import DisplayWeather from './DisplayWeather';
 
 class App extends Component {
 
   state = {
-    zipcode: null,
-    lat: null,
+    zipcode: null, 
+    lat: null, 
     lon: null
   }
 
@@ -17,6 +18,28 @@ class App extends Component {
     })
   }
 
+  getWeather = () => {
+    if (this.state.lat != null && this.state.lon != null) {
+      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${ this.state.lat }&lon=${ this.state.lon }&appid=c7e4b6ffdc34413cd5d2c37f336b78f4`)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(JSON.stringify(myJson));
+        });
+    } else if ( this.state.zipcode != null) {
+      fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${ this.state.zipcode },${ "US" }&appid=c7e4b6ffdc34413cd5d2c37f336b78f4`)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(JSON.stringify(myJson));
+        });
+    }  else {
+      console.log("Need Data");
+    } 
+  }
+
   render() {
     return (
       <div id="container" className="App">
@@ -26,8 +49,10 @@ class App extends Component {
         <p>{ this.state.lat }</p>
         <p>{ this.state.lon }</p>
         <p>{ this.state.zipcode }</p>
-
         
+        { this.getWeather() }
+
+        <DisplayWeather />
 
       </div>
     );
---
+++ src/DisplayWeather.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DisplayWeather extends Component {
+    render() {
+        return (
+            null
+        )
+    }
+}
+
+export default DisplayWeather;
