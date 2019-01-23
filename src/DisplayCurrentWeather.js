bbdcd2c417423bc55f246e24d0f47052febf68d9
Rashad Balashov <rashad.balashov@gmail.com>
2019-01-22 20:42:44 -0500

    Logo and Temperature Correct, Now Must Fix Location Button

--- src/DisplayCurrentWeather.js
+++ src/DisplayCurrentWeather.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './DisplayCurrentWeather.css';
+import sun from './images/sun.png';
 
 function DisplayCurrentWeather(weatherData) {
 
@@ -10,18 +11,17 @@ function DisplayCurrentWeather(weatherData) {
     var maxTemp = main.temp_max;
     var conditions = weather[0].main;
 
-    console.log(weatherData);
-    console.log();
-
     return(
         <div id="currentWeatherCointainer">
+            <p id="locationName">{ name }, { country }</p>
 
             {/* Display Icon Based On Weather */}
+            <div className="tempAndIcon">
+                <img src={ sun } alt="sunny" id="weatherIcon"/>
+                <p id="locationTemp">{ Math.round(mainTemp) }&#176;</p>
+            </div>
             
-
-            <p id="locationTemp">{ Math.round(mainTemp) }&#176;</p>
             <p id="minAndMaxTemp">{ conditions } | { Math.round(minTemp) }&#176; | { Math.round(maxTemp) }&#176;</p>
-            <p id="locationName">{ name }, { country }</p>
         </div>
     )
 } 
